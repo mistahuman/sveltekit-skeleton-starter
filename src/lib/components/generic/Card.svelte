18c571e85a39f83cc7e82bbc5614f2222960bba3
Marco Lanconelli <marko_lan@hotmail.it>
2026-05-10 13:27:51 +0200

    update card (wip_

--- src/lib/components/generic/Card.svelte
+++ src/lib/components/generic/Card.svelte
@@ -3,19 +3,25 @@
 
 	let {
 		title = '',
+		content = '',
 		children
 	}: {
 		title?: string;
+		content?: string;
 		children?: Snippet;
 	} = $props();
 </script>
 
-<div class="w-full card border-[1px] border-surface-200-800 preset-filled-surface-100-900 p-4">
-	<header class="h3">
+<div class="card p-4 preset-outlined-primary-500">
+	<header class="text-center h3">
 		{title}
 	</header>
 	<hr class="divider" />
 	<article class="p-4">
-		{@render children?.()}
+		<!-- {@render children?.()} -->
+		{content}
 	</article>
+	<footer class="flex items-center justify-between gap-4 p-4">
+		<small class="opacity-60">On {new Date().toLocaleDateString()}</small>
+	</footer>
 </div>
