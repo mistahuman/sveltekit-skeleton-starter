2be4d1c6a0d8c691eac2839880348e91f2566092
Marco Lanconelli <marko_lan@hotmail.it>
2026-06-01 22:04:43 +0200

    fix

--- src/routes/exampleitems/+page.ts
+++ src/routes/exampleitems/+page.ts
@@ -1,6 +1,8 @@
 import type { PageLoad } from './$types';
 import { createExampleItemsApi } from '$lib/api/client';
 
+export const prerender = false;
+
 export const load: PageLoad = async ({ fetch }) => {
 	const items = await createExampleItemsApi(fetch).list();
 	return { items };
